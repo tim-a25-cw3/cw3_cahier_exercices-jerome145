876e4e1de648189b742d36ebf02f8881ff41c0cc
jerome145 <jerome.lessard12@gmail.com>
2025-11-17 12:01:02 -0500

    installation des swiper

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,27 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
+    this.initSwiperPagination();
   }
 
   init() {
     Icons.load();
+  }
+
+  initSwiperPagination() {
+    const target = document.querySelector('.js-swiper');
+    if (target) {
+      const swiper = new Swiper(target, {
+        speed: 400,
+
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
